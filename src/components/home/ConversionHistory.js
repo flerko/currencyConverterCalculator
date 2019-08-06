ebf29fc6bb26032843d670e921594a861697377b
Evgeny Plyusnin <forao22@mail.ru>
2019-08-06 20:14:41 +0600

    Add converter popup

--- src/components/home/ConversionHistory.js
+++ src/components/home/ConversionHistory.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import businessBank from '../../assets/images/home/business-bank-investor.png';
+import CurrencyConverter from '../modals/CurrencyConverter';
 
 class ConversionHistory extends Component {
+  state = {
+    showCurrencyConverter: true
+  };
+
   render() {
     return (
       <div className="conversion-history">
+        <CurrencyConverter show={this.state.showCurrencyConverter} />
         <h2 className='conversion-history__title'>История конвертации</h2>
         <ul className='conversion-history__list'>
           <li className='conversion-history__item'>220 USD = 14221.3 RUB</li>
---
+++ src/components/modals/CurrencyConverter.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class CurrencyConverter extends Component {
+  render() {
+    return (
+      this.props.show ?
+        <div className="currency-converter">
+          <h2 className='currency-converter__title'>Конвертер валют</h2>
+          <div className='currency-converter__selection-container'>
+            <div className='currency-converter__selection-block'>
+              <input type="text"/>
+              <select>
+                <option value="USD">Доллар США, USD</option>
+                <option value="EUR">Евро, EUR</option>
+                <option value="GBP">Фунт стерлингов, GBP</option>
+              </select>
+            </div>
+            <div className='currency-converter__selection-block'>
+              <input type="text"/>
+              <select>
+                <option value="USD">Доллар США, USD</option>
+                <option value="EUR">Евро, EUR</option>
+                <option value="GBP">Фунт стерлингов, GBP</option>
+              </select>
+            </div>
+          </div>
+        </div> : null
+    );
+  }
+}
+
+CurrencyConverter.propTypes = {
+  show: PropTypes.bool,
+};
+
+export default CurrencyConverter;
